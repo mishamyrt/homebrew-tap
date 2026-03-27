218af4f45a87d7a76bf3f9d5a4be2452c05f548e
mishamyrt <misha@myrt.co>
2026-03-27 12:42:11 +0300

    style: fix bose-nc

--- Formula/bose-nc.rb
+++ Formula/bose-nc.rb
@@ -1,26 +1,23 @@
 class BoseNc < Formula
   desc "CLI tool for controlling Bose noise cancelling headphones"
   homepage "https://github.com/mishamyrt/bose-nc"
-  version "0.2.1"
   license "MIT"
 
-  on_arm do
+  depends_on :macos
+
+  if Hardware::CPU.arm?
     url "https://github.com/mishamyrt/bose-nc/releases/download/v0.2.1/bose-nc-aarch64-apple-darwin.tar.xz"
     sha256 "30350e841618434499db3c605ed88be1235ccc7c48e3c12afa5516e01fccbdf8"
-  end
-
-  on_intel do
+  else
     url "https://github.com/mishamyrt/bose-nc/releases/download/v0.2.1/bose-nc-x86_64-apple-darwin.tar.xz"
     sha256 "330b9563f7113b5e5ea774239e1621b4bfb6c50657863118274af0ddcc3ed0c4"
   end
-
-  depends_on :macos
 
   def install
     bin.install "bose-nc"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/bose-nc --version")
+    assert_match version.to_s, shell_output("#{bin}/bose-nc version")
   end
 end
