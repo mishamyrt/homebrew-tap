f985a1ef5e342a2508634ed69655f754b6194c90
mishamyrt <misha@myrt.co>
2026-04-12 02:19:36 +0300

    chore: update repomop to v0.7.0

--- Formula/repomop.rb
+++ Formula/repomop.rb
@@ -6,11 +6,11 @@ class Repomop < Formula
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/mishamyrt/repomop/releases/download/v0.6.1/repomop_darwin_arm64.tar.gz"
-    sha256 "20083dd6532c41634917b64bd1e2ec763730507984a2d9d10f5a92e82797b8a9"
+    url "https://github.com/mishamyrt/repomop/releases/download/v0.7.0/repomop_darwin_arm64.tar.gz"
+    sha256 "a657bc09d371a95a50f86e4aad00af0dabe7619a8f8ff331d4a37ae9b6ecf74d"
   else
-    url "https://github.com/mishamyrt/repomop/releases/download/v0.6.1/repomop_darwin_amd64.tar.gz"
-    sha256 "fa6e3d74c20e1dcc52e1de1bb45b31f51e4f822ee6c9b896f526814e6ab95fca"
+    url "https://github.com/mishamyrt/repomop/releases/download/v0.7.0/repomop_darwin_amd64.tar.gz"
+    sha256 "ec552503f57bce0a5cd4f47dd9f47d1c723172f2ee0da223a7c65c618947f3f8"
   end
 
   def install
